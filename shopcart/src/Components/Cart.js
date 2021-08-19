7feee2c44b124b5b38101292307c69a1cde55635
Meraki19 <88908520+Meraki19@users.noreply.github.com>
2021-08-20 02:57:41 +0530

    code clean up

--- shopcart/src/Components/Cart.js
+++ shopcart/src/Components/Cart.js
@@ -3,20 +3,20 @@ import PageTitle from './PageHeading';
 import { Link } from 'react-router-dom';
 
 const Cart = (props) => {
-   
+
     const TotalAmountStyles = {
         textAlign: 'right',
-        fontSize:'36px',
-        marginTop:'20px',
-        fontWeight:'500',
+        fontSize: '36px',
+        marginTop: '20px',
+        fontWeight: '500',
         paddingRight: '20px'
 
     }
     function HandleRemoveProductOnClick(productId) {
-       alert("Do you Want to remove this product ?? ")
-       props.removeFromCart(productId)
-     
-         
+        alert("Do you Want to remove this product ?? ")
+        props.removeFromCart(productId)
+
+
     }
 
     var totalAmount = props.itemsInCart.reduce((a, v) => a + v.price, 0)
@@ -24,30 +24,30 @@ const Cart = (props) => {
         <div className="container cart-container">
             <div className="d-flex  align-all-center ">
                 <PageTitle heading="My Cart" />
-                {props.itemsInCart.length==0 ?<h4> No items in your Cart !! Epxlore products in home page</h4>
-                :<div className="w-100 d-flex  justify-content-end">
-                <Link to="/"   style={{textDecoration: 'none',marginRight:'20px'}}><button className="btn-primary"> Back</button></Link>
-                <Link to="/" style={{textDecoration: 'none'}} ><button className=" btn-success"> Place Order</button></Link>
-                </div>}
-                
+                {props.itemsInCart.length == 0 ? <h4> No items in your Cart !! Epxlore products in home page</h4>
+                    : <div className="w-100 d-flex  justify-content-end">
+                        <Link to="/" style={{ textDecoration: 'none', marginRight: '20px' }}><button className="btn-primary"> Back</button></Link>
+                        <Link to="/" style={{ textDecoration: 'none' }} ><button className=" btn-success"> Place Order</button></Link>
+                    </div>}
+
             </div>
 
             <div>
                 {
-                    props.itemsInCart.map(c => 
-                    
-                    <div className="cart-item-name d-flex">
-                        <div className="w-100"><h5>{c.title}</h5><p style={{marginBottom:'0'}}>{c.category}</p></div> 
-                       
-                       <div className=" cart-price-item">
-                       <p style={{fontSize:'30px',marginRight:'30px',marginBottom:'0'}}>${c.price}</p>
-                            <i onClick={()=>HandleRemoveProductOnClick(c.id)} style={{color:'red',fontSize:'28px'}} className="icon-container fa fa-trash-o"></i>
-                       </div>
+                    props.itemsInCart.map(c =>
+
+                        <div className="cart-item-name d-flex">
+                            <div className="w-100"><h5>{c.title}</h5><p style={{ marginBottom: '0' }}>{c.category}</p></div>
+
+                            <div className=" cart-price-item">
+                                <p style={{ fontSize: '30px', marginRight: '30px', marginBottom: '0' }}>${c.price}</p>
+                                <i onClick={() => HandleRemoveProductOnClick(c.id)} style={{ color: 'red', fontSize: '28px' }} className="icon-container fa fa-trash-o"></i>
+                            </div>
                         </div>
-                       
-                        )
+
+                    )
                 }
-                {props.itemsInCart.length==0 ?null: <h3 style={TotalAmountStyles}>Total Amount : ${totalAmount}</h3>}
+                {props.itemsInCart.length == 0 ? null : <h3 style={TotalAmountStyles}>Total Amount : ${totalAmount}</h3>}
             </div>
         </div>
     )
